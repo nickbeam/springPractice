315b600e3fc6c1df452bc198ab16db98c792693a
dvinin <dvininnick@gmail.com>
2020-01-29 17:54:48 +0300

    Complete Task 2: init and populate meals table at sql_db

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
@@ -23,3 +24,16 @@ CREATE TABLE user_roles
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE MEALS
+(
+    id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    user_id          INTEGER NOT NULL
+        CONSTRAINT meals_user_id_fk
+            REFERENCES users
+            ON DELETE CASCADE,
+    date_time           TIMESTAMP   NOT NULL,
+    description         VARCHAR     NOT NULL,
+    calories            INTEGER     NOT NULL
+);
+CREATE UNIQUE INDEX IF NOT EXISTS meals_id_uindex ON MEALS (ID);
