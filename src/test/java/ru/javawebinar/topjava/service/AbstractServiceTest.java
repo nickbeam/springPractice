705b23f6369c2fbca31110e294fdc20f9aee32d0
nick <dvininnick@gmail.com>
2020-02-27 20:31:08 +0300

    Print colored text console

--- src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
@@ -13,8 +13,12 @@ import java.util.Calendar;
 
 public abstract class AbstractServiceTest {
     private double timeStart;
-    private SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss.SSS");
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss.SSS");
     private static StringBuilder stringBuilder = new StringBuilder();
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_RESET = "\u001B[0m";
+    private static final String LINE = ANSI_GREEN + "+--------------+--------------------------------------------+-----------------+\n" + ANSI_RESET;
+    private static final String TITLE = ANSI_GREEN + "|  Start time  |                   Test name                |  Duration (sec) |\n" + ANSI_RESET;
 
     @Rule
     public TestRule watcher = new TestWatcher() {
@@ -22,30 +26,33 @@ public abstract class AbstractServiceTest {
             String leftAlignFormat = "| %-12s | %-42s |";
             timeStart = System.currentTimeMillis();
             Calendar cal = Calendar.getInstance();
-            stringBuilder.append(String.format(leftAlignFormat, dateFormat.format(cal.getTime()), description.getMethodName()));
+            stringBuilder
+                    .append(ANSI_GREEN)
+                    .append(String.format(leftAlignFormat, dateFormat.format(cal.getTime()), description.getMethodName()));
         }
 
         protected void finished(Description description) {
             String rightAlignFormat = " %-15s |%n";
             double timeEnd = System.currentTimeMillis();
             double seconds = (timeEnd - timeStart) / 1000.0;
-            stringBuilder.append(String.format(rightAlignFormat, new DecimalFormat("0.000").format(seconds)));
-            System.out.println("+--------------+--------------------------------------------+-----------------+");
-            System.out.println(stringBuilder.substring(stringBuilder.length() - 81, stringBuilder.length() - 1));
-            System.out.println("+--------------+--------------------------------------------+-----------------+");
+            stringBuilder
+                    .append(String.format(rightAlignFormat, new DecimalFormat("0.000").format(seconds)))
+                    .append(ANSI_RESET);
+
+            System.out.print(LINE);
+            System.out.print(ANSI_GREEN + stringBuilder.substring(stringBuilder.length() - LINE.length() - 1, stringBuilder.length()) + ANSI_RESET);
+            System.out.print(LINE);
         }
     };
 
     @BeforeClass
     public static void tableHead() {
-        stringBuilder.append("+--------------+--------------------------------------------+-----------------+\n");
-        stringBuilder.append("|  Start time  |                   Test name                |  Duration (sec) |\n");
-        stringBuilder.append("+--------------+--------------------------------------------+-----------------+\n");
+        stringBuilder.append(LINE).append(TITLE).append(LINE);
     }
 
     @AfterClass
     public static void printTiming() {
-        stringBuilder.append("+--------------+--------------------------------------------+-----------------+\n");
+        stringBuilder.append(LINE);
         System.out.print(stringBuilder);
     }
 }
